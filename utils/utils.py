4d5632cc8a87498dca6a64a83ba18226dcbe6f78
m-aliabbas <vynz420@gmail.com>
2023-07-14 10:38:54 +0500

    fronend

--- utils/utils.py
+++
@@ -1,9 +0,0 @@
-def extract_phrases(input_dict):
-    output_dict = {}
-    for key, value in input_dict.items():
-        inner_key = next(iter(value))
-        output_dict[key] = value[inner_key]
-    return output_dict
-
-
-    
